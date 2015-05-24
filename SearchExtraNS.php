6f7f8d8cfe02be33fcce554f640051fd6a89150c
Lewis Cawte <lewis@lewiscawte.me>
2015-05-24 18:15:51 +0200

    Convert to extension registration
    Bug: T87971
    Change-Id: I0747c822bfabef7b3acdad44e1bbf13a79daa427

--- SearchExtraNS.php
+++ SearchExtraNS.php
@@ -1,44 +1,13 @@
 <?php
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'SearchExtraNS' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['SearchExtraNS'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for SearchExtraNS extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the SearchExtraNS extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'SearchExtraNS',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:SearchExtraNS',
-	'descriptionmsg' => 'searchextrans-desc',
-	'author' => array( 'Roland Unger', 'Hans Musil' ),
-);
-
-$wgMessagesDirs['SearchExtraNS'] = __DIR__ . '/i18n';
-
-$wgSearchExtraNamespaces = false;
-
-class SearchExtraNS {
-
-	/**
-	 * @param $term string
-	 * @param $title Title
-	 * @return bool
-	 */
-	public static function NearMatch( $term, &$title ) {
-		global $wgSearchExtraNamespaces;
-
-		if( !is_array( $wgSearchExtraNamespaces ) ){
-			return true;
-		}
-
-		foreach( $wgSearchExtraNamespaces as $ens ) {
-			$title = Title::newFromText( $term, $ens );
-			if ( $title && $title->exists() ) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-}
-
-$wgHooks['SearchAfterNoDirectMatch'][] = 'SearchExtraNS::NearMatch';

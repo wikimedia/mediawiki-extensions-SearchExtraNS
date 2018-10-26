dadc27731131c7d416e863382a88b7cb1d83585c
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:54:25 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: Iefac0b467f95747ce7c9d48ae8dbae0eddb2110d

--- SearchExtraNS.php
+++ SearchExtraNS.php
@@ -3,11 +3,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SearchExtraNS' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['SearchExtraNS'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for SearchExtraNS extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the SearchExtraNS extension requires MediaWiki 1.25+' );
